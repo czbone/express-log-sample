e5216444052621f4feb2c4aebeb2ac7dbab62a48
naoki hirata <naoki@magic3.org>
2023-10-23 10:59:06 +0900

    パッケージ更新。

--- tools/clean.mjs
+++ tools/clean.mjs
@@ -1,3 +1,3 @@
-import { rm } from "node:fs/promises";
+import { rm } from "node:fs/promises"
 
-await rm("dist", { recursive: true, force: true });
+await rm("dist", { recursive: true, force: true })
